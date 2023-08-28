4450fae4d33750aa3d430baa1238dac5e2555dc2
JTHaugseth <97683128+JTHaugseth@users.noreply.github.com>
2023-08-28 16:29:20 +0200

    HTML tasks 1,2 V1 + browsermockup V1 -- complete

--- src/components/HTML/Task1HTML.js
+++ src/components/HTML/Task1HTML.js
@@ -1,10 +1,47 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import ImageHTML from '../../images/HTML.JPG';
 
 function Task1HTML() {
   return (
     <div className="container-fluid">
-      {/* Content for Intro to HTML will go here */}
-      <h1>html-dokumentets-grunnstruktur</h1>
+      {/* Title */}
+      <h1 className='mb-5'>HTML-dokumentets grunnstruktur</h1>
+      
+      {/* 1x1 Grid */}
+      <div className="row">
+        {/* Text Column */}
+        <div className="col-md-6">
+          <p><strong>&lt;!DOCTYPE html&gt;:</strong> Dette er dokumenttypedefinisjonen og forteller nettleseren at dokumentet er skrevet i HTML5. Det bør alltid være den første linjen i HTML-dokumentet ditt.</p>
+          
+          <p><strong>&lt;html lang="en"&gt;:</strong> Dette elementet omslutter alle innholdselementene på nettsiden din. lang="en" attributtet forteller nettleseren at innholdet er på engelsk, noe som kan være nyttig for skjermlesere og søkemotorer.</p>
+          
+          <p><strong>&lt;head&gt;:</strong> Dette elementet inneholder metainformasjon om dokumentet og andre ressurser som stilark eller skripter som nettsiden bruker. Innholdet i &lt;head&gt; vises ikke på selve nettsiden.</p>
+          
+          <p><strong>&lt;meta charset="UTF-8"&gt;:</strong> Dette angir tegnkodingen for dokumentet, som er viktig for korrekt tekstvisning.</p>
+          
+          <p><strong>&lt;meta name="viewport" content="width=device-width, initial-scale=1.0"&gt;:</strong> Dette hjelper med å gjøre nettsiden responsiv, slik at den ser bra ut på forskjellige enheter.</p>
+          
+          <p><strong>&lt;title&gt;Document&lt;/title&gt;:</strong> Dette setter tittelen på nettsiden, som vises i nettleserens tittellinje eller fanekort.</p>
+          
+          <p><strong>&lt;body&gt;:</strong> Dette er hoveddelen av HTML-dokumentet og inneholder alt innholdet som skal vises på nettsiden, som tekst, bilder, lenker, etc.</p>
+        </div>
+        
+        {/* Image Column */}
+        <div className="col-md-6">
+          <img src={ImageHTML} alt="Placeholder" className="img-fluid"/>
+        </div>
+      </div>
+
+      {/* Buttons at the bottom */}
+      <div className="row mt-5 justify-content-center">
+        <div className="col-auto">
+          <Link to="/" className="btn btn-secondary">Tilbake</Link>
+        </div>
+        <div className="col-auto">
+          <Link to="/tekst-og-overskrifter" className="btn btn-primary">Neste</Link>
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/HTML/Task2HTML.js
+++ src/components/HTML/Task2HTML.js
@@ -1,10 +1,57 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import Task2ExampleCode from '../../images/Task2ExampleCode.JPG';
+import Task2ExampleResult from '../../images/Task2ExampleResult.JPG';
+import '../../styles/browser-mockup.css';
 
 function Task2HTML() {
   return (
     <div className="container-fluid">
-      {/* Content for Intro to HTML will go here */}
-      <h1>tekst-og-overskrifter</h1>
+      <h1 className='mb-5'>Tekst og Overskrifter</h1>
+
+      {/* 2x2 Grid */}
+      <div className="row">
+        {/* Top-left cell */}
+        <div className="col-md-6">
+          <p>I HTML bruker vi ulike typer "tags" for å markere tekst. Dette gir nettleseren beskjed om hvordan teksten skal vises. For eksempel, hvis vi ønsker å lage et nytt avsnitt, kan vi bruke &lt;p&gt;-taggen, som står for "paragraf". Hvis vi ønsker å lage en overskrift, kan vi bruke &lt;h1&gt;, &lt;h2&gt;, &lt;h3&gt; osv., der tallet indikerer nivået på overskriften.</p>
+          <pre>&lt;p&gt;Dette er en paragraf.&lt;/p&gt;</pre>
+          <pre>&lt;h1&gt;Dette er en hovedoverskrift&lt;/h1&gt;</pre>
+          <pre>&lt;h2&gt;Dette er en underoverskrift&lt;/h2&gt;</pre>
+        </div>
+
+        {/* Top-right cell */}
+        <div className="col-md-6">
+          <img src={Task2ExampleCode} alt="Placeholder" className="img-fluid"/>
+        </div>
+      </div>
+
+      <div className="row mt-5">
+        {/* Bottom-left cell */}
+        <div className="col-md-6">
+          <h2>Oppgave</h2>
+          <p>1. Lag en enkel HTML-side med en hovedoverskrift som sier "Min første nettside".<br/>
+          2. Legg til en paragraf under overskriften som forklarer hva nettsiden handler om.<br/>
+          3. Bruk en &lt;h2&gt;-tag for å legge til en underoverskrift som sier "Dagens Dato".<br/>
+          4. Avslutt med en paragraf som forteller hvilken dato det er i dag.</p>
+        </div>
+
+        {/* Bottom-right cell */}
+        <div className="col-md-6">
+          <div className="browser-mockup">
+            <img src={Task2ExampleResult} alt="Webpage screenshot" className="img-fluid" />
+          </div>
+        </div>
+      </div>
+
+      {/* Buttons at the bottom */}
+      <div className="row mt-5 justify-content-center">
+        <div className="col-auto">
+          <Link to="/html-dokumentets-grunnstruktur" className="btn btn-secondary">Tilbake</Link>
+        </div>
+        <div className="col-auto">
+          <Link to="/lister-og-lenker" className="btn btn-primary">Neste</Link>
+        </div>
+      </div>
     </div>
   );
 }
