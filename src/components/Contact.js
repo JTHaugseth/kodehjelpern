738242d1a45c8221616475a4611f9f20cfca8520
JTHaugseth <97683128+JTHaugseth@users.noreply.github.com>
2023-09-05 12:23:22 +0200

    Updated contact form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,12 +1,25 @@
 import React from 'react';
 
-function Task1HTML() {
+function Contact() {
+  const yourEmail = "kodehjelpern@gmail.com";
+
   return (
-    <div className="container-fluid">
-      {/* Content for Intro to HTML will go here */}
-      <h1>Not yet implemented</h1>
+    <div className="d-flex flex-column justify-content-start align-items-center vh-100" style={{background:  'rgb(238, 238, 238)'}}>
+      <h1 className="mb-4 text-center"style={{margin:  '5%'}} >Kontakt utvikleren</h1>
+      <p className="mb-4 text-center">{yourEmail}</p>
+      <div className="text-center mb-4" style={{ maxWidth: '200px', width: '100%' }}>
+        <button 
+          type="button" 
+          className="btn btn-primary w-100"
+          onClick={() => {
+            window.location.href = `mailto:${yourEmail}?subject=Kontakt`;
+          }}
+        >
+          Send Epost
+        </button>
+      </div>
     </div>
   );
 }
 
-export default Task1HTML;
+export default Contact;
